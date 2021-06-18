df6518d50cb99e2c7708d4f28ac79996a697d07a
nilaybesli <nil.besli@hotmail.com>
2021-06-18 14:47:39 +0300

    last commit

--- main.js
+++ main.js
@@ -5,17 +5,17 @@
 function fotoChange(num){
   var foto = 'img/img' + num + '.jpg'
   document.getElementById("mainImg").src = foto;
- }
+}
 
  //muzik
 
- function togglePlay() {
+function togglePlay() {
   var x = document.getElementById("myAudio"); 
   
-     if (x.paused) {
+    if (x.paused) {
          x.play();
-     }
-     else {
+    }
+    else {
          x.pause();
-     }
+    }
  };
